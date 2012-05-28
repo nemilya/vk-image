cd2caefa9ae260a1b58a67e1e2ea31a3b3871904
Ilya Nemihin <nemilya@gmail.com>
2012-05-28 19:54:31 +0600

    first commit

---
+++ app.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+
+require 'sinatra'
+require 'yaml'
+require 'omniauth-vkontakte'
+require 'vkontakte_api'
+require 'blitline'
+
+enable :sessions
+
+require 'init'
+
+before do
+  @app = VkontakteApi::Client.new(session[:token]) if session[:token]
+end
+
+get '/' do
+  erb :index
+end
+
+get '/logout' do
+  session[:token] = nil
+  session[:name] = nil
+  redirect '/'
+end
+
+get '/auth/:name/callback' do
+  auth_hash = request.env['omniauth.auth']
+  session[:token] = auth_hash[:credentials][:token]
+  session[:name] = auth_hash[:info][:name]
+  redirect '/'
+end
---
+++ init.rb
@@ -0,0 +1,38 @@
+# by default
+config_path = File.join("config", "oauth.yml")
+
+configure :development do
+  # в режиме разрабокти используем oauth.local.yml конфиг файл
+  config_path = File.join("config", "oauth.local.yml")
+
+  # убираем проверку сертификата иначе были сложности
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+end
+
+
+if File.exists?(config_path)
+  oauth_settings = YAML.load(File.read(config_path))
+
+  if oauth_settings['vkontakte']
+    ENV['API_KEY']    ||= oauth_settings['vkontakte']['api_key']
+    ENV['API_SECRET'] ||= oauth_settings['vkontakte']['api_secret']
+  end
+
+  use OmniAuth::Builder do
+    if ENV['API_KEY']
+      provider :vkontakte, ENV['API_KEY'], ENV['API_SECRET'],
+        :scope => 'friends,audio,photos', :display => 'popup'
+    end
+  end
+end
+
+
+# blitline
+config_path = File.join("config", "blitline.yml")
+
+if File.exists?(config_path)
+  blitline_settings = YAML.load(File.read(config_path))
+  if blitline_settings['blitline']
+    ENV['BLITLINE_APPLICATION_ID'] ||= blitline_settings['blitline']['application_id']
+  end
+end
